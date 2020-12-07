40aa6835493d42afa43bf583a1cc7f326dabc26b
frabatx <francesco.battista@studenti.unitn.it>
2020-12-07 22:53:53 +0100

    Aggiunto geojson comuni

--- newSite/js/mappaComuni.js
+++ newSite/js/mappaComuni.js
@@ -2,7 +2,7 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZnJhYmF0eCIsImEiOiJja2diZWVyeXEwZ2F3MnNwZHVsYm4ydXphIn0.vMjCxPk6BAzzyXe81jTmMg';
 
 var map = new mapboxgl.Map({
-    container: 'map',
+    container: 'map-comuni',
     style: 'mapbox://styles/frabatx/ckidhutth015z19o0bltl5q0m', // stylesheet location
     center: [11.08, 46.066],
     zoom: 11// starting zoom
@@ -49,35 +49,11 @@ map.on('load', function(){
     },'waterway-label'
     );
 
-    // FUNZIONE DI PING SULLA MAPPA CON INFO SUL NOME DEL QUARTIERE (DISMESSA)
-    // map.on('click', 'quartieri-fill', function (e) {
-    //     new mapboxgl.Popup()
-    //         .setLngLat(e.lngLat)
-    //         .setHTML(e.features[0].properties.nome_quart)
-    //         .addTo(map);
-    //     });
-
-    // map.on('mouseenter', 'quartieri-fill', function () {
-    //     map.getCanvas().style.cursor = 'pointer';
-    // });
-             
-        
-    // map.on('mouseleave', 'quartieri-fill', function () {
-    //     map.getCanvas().style.cursor = '';
-    // });
-
-    // 1,'#ffe0b2',
-    // 20,'#',
-    // 50,'#',
-    // 100,'#',
-    // 200,'#',
-    // 400,'#',
-    // 1000,'#',
 
 
     var layers = ['1-20', '20-50', '50-100', '100-200', '200-400', '400-1000', '1000+'];
     var colors = ['#ffe0b2', '#ffb74d', '#ffa726', '#ff9800', '#f57c00', '#e65100', '#bf360c'];
-    var legend_comuni = document.getElementById('legend-comuni')
+    
     for (var i = 0; i < layers.length; i++) {
         var layer = layers[i];
         var color = colors[i];
@@ -90,7 +66,7 @@ map.on('load', function(){
         value.innerHTML = layer;
         item.appendChild(key);
         item.appendChild(value);
-        legend_comuni.appendChild(item);
+        legend.appendChild(item);
     }
 
     map.on('mousemove', function(e) {
@@ -99,9 +75,9 @@ map.on('load', function(){
         });
         
         if (states.length > 0) {
-          document.getElementById('pd').innerHTML = "<h4><strong>" + states[0].properties.nome_quart + "</strong></h4><p><strong><em>" + states[0].properties.counts + "</strong> articoli</em></p>";
+          document.getElementById('pd-comuni').innerHTML = "<h4><strong>" + states[0].properties.nome_quart + "</strong></h4><p><strong><em>" + states[0].properties.counts + "</strong> articoli</em></p>";
         } else {
-          document.getElementById('pd').innerHTML = '<p>Passa il mouse sui quartieri!</p>';
+          document.getElementById('pd-comuni').innerHTML = '<p>Passa il mouse sui quartieri!</p>';
         }
     });
 
